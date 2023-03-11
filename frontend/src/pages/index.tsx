8c1ab37a1cf348427aaf24dea15bce92d3ac8c78
nodari georges <ngeorges.dev@gmail.com>
2023-03-11 13:40:22 +0100

    addng clerk middleware refactor public static file

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,40 @@
 import Layout from "@/components/layouts/layout";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Layout>
         <h1 className="text-3xl font-bold underline">Hello world</h1>
+        <a href="#" className="group relative block bg-black">
+          <Image
+            alt="Developer"
+            src="/images/sf.webp"
+            width={1920}
+            height={1080}
+            className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+          />
+
+          <div className="relative p-4 sm:p-6 lg:p-8">
+            <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
+              Developer
+            </p>
+
+            <p className="text-xl font-bold text-white sm:text-2xl">
+              Tony Wayne
+            </p>
+
+            <div className="mt-32 sm:mt-48 lg:mt-64">
+              <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                <p className="text-sm text-white">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Omnis perferendis hic asperiores quibusdam quidem voluptates
+                  doloremque reiciendis nostrum harum. Repudiandae?
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
       </Layout>
     </>
   );
