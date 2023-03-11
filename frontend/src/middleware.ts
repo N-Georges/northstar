7d23c89608d7fc6a47d60bb38ae5b21a6856c0a6
nodari georges <ngeorges.dev@gmail.com>
2023-03-11 01:12:05 +0100

    refactor clerk

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,30 +1,30 @@
-import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ["/", "/sign-in*", "/sign-up*"];
+// const publicPaths = ["/", "/sign-in*", "/sign-up*"];
 
-const isPublic = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-};
+// const isPublic = (path: string) => {
+//   return publicPaths.find((x) =>
+//     path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+//   );
+// };
 
 export default withClerkMiddleware((req: NextRequest) => {
-  if (isPublic(req.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-  // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(req);
+  // if (isPublic(req.nextUrl.pathname)) {
+  //   return NextResponse.next();
+  // }
+  // // if the user is not signed in redirect them to the sign in page.
+  // const { userId } = getAuth(req);
 
-  if (!userId) {
-    // redirect the users to /pages/sign-in/[[...index]].ts
+  // if (!userId) {
+  //   // redirect the users to /pages/sign-in/[[...index]].ts
 
-    const signInUrl = new URL("/sign-in", req.url);
-    signInUrl.searchParams.set("redirect_url", req.url);
-    return NextResponse.redirect(signInUrl);
-  }
+  //   const signInUrl = new URL("/sign-in", req.url);
+  //   signInUrl.searchParams.set("redirect_url", req.url);
+  //   return NextResponse.redirect(signInUrl);
+  // }
   return NextResponse.next();
 });
 
