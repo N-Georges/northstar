9abcb37a804b806bc1112ec448049ae660312f27
nodari georges <ngeorges.dev@gmail.com>
2023-03-11 01:19:10 +0100

    refactor clerk

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider>
       <Component {...pageProps} />
     </ClerkProvider>
   );
